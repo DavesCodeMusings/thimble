3111d22e13bb64d50e3f88bcd6abaea28e173b41
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-10-10 23:02:05 -0500

    Create thimble.py

---
+++ thimble.py
@@ -0,0 +1,59 @@
+"""
+  Thimble -- A tiny web framework in the spirit of Flask, but scaled down to run on microcontrollers.
+"""
+
+from socket import socket, getaddrinfo, AF_INET, SOCK_STREAM
+
+class Thimble:
+    def __init__(self, default_content_type='text/html'):
+        self.req_buffer_size = 1024
+        self.routes = {}
+        self.default_content_type = default_content_type
+
+    def route(self, path):
+        def add_route(func):
+            self.routes[path] = func
+        return add_route
+
+    def run(self, ip='0.0.0.0', port=80):
+        self.ip = ip
+        self.port = port
+        addr = getaddrinfo(self.ip, self.port)[0][-1]
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.bind(addr)
+        sock.listen()
+        print(f'Listening on {self.ip}:{self.port}')
+
+        while (True):
+            conn, addr = sock.accept()
+            print(f'Connection from: {addr}')
+            req = { 'is_valid': True }
+            try:
+                req_buffer = conn.recv(self.req_buffer_size).decode('utf8').split('\r\n')
+                print(req_buffer)
+                req_length = len(req_buffer)  # Number of lines.
+                req['method'], req['url'], req['http_version'] = req_buffer[0].split(' ', 2)  # Example first line: GET /page.html HTTP/1.1
+                req['header'] = {}
+                for i in range(1, req_length - 1):
+                    if (req_buffer[i] == ''):  # Blank line signifies the end of headers.
+                        break
+                    else:
+                        name, value = req_buffer[i].split(':', 1)
+                        req['header'][name.strip()] = value.strip()
+                req['body'] = req_buffer[req_length - 1]  # Last line is the body (or blank if no body.)
+
+            except:
+                req['is_valid'] = False
+
+            if (req['is_valid']):
+                if (req['url'] in self.routes and callable(self.routes[req['url']])):  # Is it a function in the route table?
+                    conn.send('HTTP/1.0 200 OK\r\n')
+                    conn.send(f'Content-type: {self.default_content_type}\r\n')  # TODO: Make response headers user definable
+                    conn.send('\r\n')
+                    conn.send(f'{self.routes[req['url']]()}\r\n')  # Send the return value from function as the response body.
+                else:
+                    conn.send('HTTP/1.0 404 Not Found\r\n\r\n')
+            else:
+                conn.send('HTTP/1.0 400 Bad Request\r\n\r\n')
+
+            conn.close()

3805f48104cae0ccce62a417317ffc6ceb20f03a
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-10-13 22:17:09 -0500

    Code clean-up

--- thimble.py
+++ thimble.py
@@ -24,7 +24,7 @@ class Thimble:
     def http_response_status(status_code):
         if (status_code not in Thimble.http_status_text):
             status_code = 500
-        return f'HTTP/1.1 {Thimble.http_status_text[status_code]}\r\n'
+        return f'HTTP/1.1 {Thimble.http_status_text[status_code]}'
 
     # Given a raw HTTP request, return a dictionary with individual elements broken out.
     # Takes a buffer containing the HTTP request sent from the client.
@@ -32,32 +32,49 @@ class Thimble:
     @staticmethod
     def parse_http_request(req_buffer):
         req = {}
-        try:
-            req_buffer_lines = req_buffer.decode('utf8').split('\r\n')
-            req['method'], req['target'], req['http_version'] = req_buffer_lines[0].split(' ', 2)  # Example first line: GET /page.html HTTP/1.1
-            if ('?' in req['target']):
-                req['path'], req['query'] = target.split('?')
-                req['query'] = '?' + req['query']  # Convention is to include the question mark.
+        req_buffer_lines = req_buffer.decode('utf8').split('\r\n')
+        req['method'], req['target'], req['http_version'] = req_buffer_lines[0].split(' ', 2)  # Example first line: GET /page.html HTTP/1.1
+        if ('?' in req['target']):
+            req['path'], req['query'] = target.split('?')
+            req['query'] = '?' + req['query']  # Convention is to include the question mark.
+        else:
+            req['path'] = req['target']
+        req['headers'] = {}
+        for i in range(1, len(req_buffer_lines) - 1):
+            if (req_buffer_lines[i] == ''):  # Blank line signifies the end of headers.
+                break
             else:
-                req['path'] = req['target']
-            req['headers'] = {}
-            for i in range(1, len(req_buffer_lines) - 1):
-                if (req_buffer_lines[i] == ''):  # Blank line signifies the end of headers.
-                    break
-                else:
-                    name, value = req_buffer_lines[i].split(':', 1)
-                    req['headers'][name.strip()] = value.strip()
-            req['body'] = req_buffer_lines[len(req_buffer_lines) - 1]  # Last line is the body (or blank if no body.)
-        except:
-            req = None
+                name, value = req_buffer_lines[i].split(':', 1)
+                req['headers'][name.strip()] = value.strip()
+        req['body'] = req_buffer_lines[len(req_buffer_lines) - 1]  # Last line is the body (or blank if no body.)
         return req
 
+    # Given a path and list of zero or more HTTP methods, add the decorated function to the route table. 
     def route(self, url_path, methods=['GET']):
         def add_route(func):
             for method in methods:
-                self.routes[method + url_path] = func
+                self.routes[method.upper() + url_path] = func  # Methods are uppercase (see RFC 9110.)
         return add_route
 
+    # Given the method and path of an HTTP request, look up the function in the route table and execute it.
+    # Returns (body, status_code) tuple. Example: ("Hello!", 200) or ("", 404) if no route or ("", 500) on error.
+    def call_route_function(self, req):
+        route_key = req['method'] + req['path']
+        res = None
+
+        if (not route_key in self.routes or not callable(self.routes[route_key])):  # No function in the route table?
+            res = Thimble.http_response_status(404), 404
+        else:
+            try:
+                res = self.routes[route_key](req)  # Execute function in routing table, passing request parameters.
+            except:
+                res = Thimble.http_response_status(500), 500
+            if (not isinstance(res, tuple)):  # User-defined functions can respond with a body, status code tuple or just a body.
+                body = res
+                res = body, 200  # Assume 200 OK when it's just a body in the response.
+        return res
+
+    # Start listening for connections.
     def run(self, host='0.0.0.0', port=80, debug=False):
         sock = socket(AF_INET, SOCK_STREAM)
         sock.bind(getaddrinfo(host, port)[0][-1])
@@ -65,39 +82,30 @@ class Thimble:
         print(f'Listening on {host}:{port}')
 
         while (True):
+            req = None
+            body = None
+            status_code = None
+            res = []
+
             conn, client = sock.accept()
             if (debug): print(f'Connection from: {client}')
-            req_buffer = conn.recv(self.req_buffer_size)
-            req = Thimble.parse_http_request(req_buffer)
-            if (debug): print(req)
-            if (req):
-                route_key = req['method'] + req['path']  # Example: 'GET/gpio/0'
-                if (debug): print(f'Looking up route by key: {route_key}')
-                if (route_key in self.routes and callable(self.routes[route_key])):  # Is it a function in the route table?
-                    res = None
-                    if (debug): print(f'Executing: {self.routes[route_key]}')
-                    try:
-                        res = self.routes[route_key](req)  # Execute function pointed to by route_key with request parameters.
-                    except:
-                        if (debug): print("Function call failed.")
-                        res = ('', 500)
-                    body = ''
-                    status_code = 200
-                    if (isinstance(res, tuple)):
-                        body, status_code = res
-                    else:
-                        body = res
-                    conn.send(f'{Thimble.http_response_status(status_code)}')  # Status line
-                    conn.send(f'Content-Length: {len(body)}\r\n')
-                    conn.send(f'Content-Type: {self.default_content_type}\r\n')
-                    conn.send(f'Server: {self.server_name}\r\n')
-                    conn.send('\r\n')  # Blank line denotes end of headers.
-                    conn.send(f'{body}\r\n')
-                else:
-                    if (debug): print('No route found.')
-                    conn.send(f'{Thimble.http_response_status(404)}')
+            try:
+                req_buffer = conn.recv(self.req_buffer_size)
+                req = Thimble.parse_http_request(req_buffer)
+            except:
+                if (debug): print('HTTP request was too long or did not parse correctly.')
+                body = 'Invalid Request'
+                status_code = 400
             else:
-                if (debug): print('HTTP request was invalid or too long')
-                conn.send(f'{Thimble.http_response_status(400)}')
-
-            conn.close()
+                if (debug): print(f'Request: {req}')
+                if (debug): print(f'Looking up function for: {req['method']} {req['path']}')
+                body, status_code = self.call_route_function(req)
+                res.append(Thimble.http_response_status(status_code))
+                res.append(f'Content-Length: {len(body)}')
+                res.append(f'Content-Type: {self.default_content_type}')
+                res.append(f'Server: {self.server_name}')
+                res.append('')
+                res.append(f'{body}')
+                if (debug): print('\r\n'.join(res))
+                conn.send('\r\n'.join(res))
+                conn.close()

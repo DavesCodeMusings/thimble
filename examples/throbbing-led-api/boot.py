78789082408f8b1a78b40a565574768db1e1ab39
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2023-03-25 08:55:31 -0500

    Create boot.py

---
+++ examples/throbbing-led-api/boot.py
@@ -0,0 +1,14 @@
+from machine import Pin, PWM
+from network import WLAN, AP_IF, AUTH_WPA2_PSK
+from config import AP_NAME, AP_PASS, GPIO
+
+led = PWM(Pin(GPIO), freq=1000)
+led.duty(512)  # Half bright
+
+print('\nStarting in wifi access point mode...')
+wlan = WLAN(AP_IF)
+wlan.active(True)
+wlan.config(authmode=AUTH_WPA2_PSK, essid=AP_NAME, password=AP_PASS)
+print(f'SSID: {AP_NAME}')
+print(f'Password: {AP_PASS}')
+print(wlan.ifconfig()[0])  # This is the device's IP address

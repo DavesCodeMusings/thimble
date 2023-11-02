e14f0e2ddc307753c84af2ebe4ccf23c871c6ad5
Dave Horton <dhorton668@outlook.com>
2023-11-02 12:23:59 -0500

    comment typo fix

--- thimble.py
+++ thimble.py
@@ -78,7 +78,7 @@ class Thimble:
         req = {}
         req_buffer_string = req_buffer.decode('utf8')
         req_buffer_lines = req_buffer_string.split('\r\n')
-        del req_buffer_string  # free up or garbage collection
+        del req_buffer_string  # free up for garbage collection
 
         req['method'], target, req['http_version'] = req_buffer_lines[0].split(
             ' ', 2)  # Example: GET /route/path HTTP/1.1
